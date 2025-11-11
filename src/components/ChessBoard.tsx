c259985a4a30080ac8f0c6c728613dc6c9bc59d5
Sultan <sultan@mse.mn>
2025-11-11 19:27:26 +0530

    updates

--- src/components/ChessBoard.tsx
+++ src/components/ChessBoard.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 import Square from './Square'
 import type { PieceType } from '../types'
 
-const API_BASE = "https://gameportfolio-f7if-6xjmctu8h-wispas-projects.vercel.app/";
+const API_BASE = "https://vercel.com/wispas-projects/gameportfolio-f7if/5PHe62awPnbk4H7FpbhuwUiu7LUs";
 
 const makeMove = async (from: string, to: string) => {
   try {
